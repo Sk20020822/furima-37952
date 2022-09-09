7c258c29fce86bc9a70e0fe501c549f13e5a0491
Sk20020822 <kaito.nike0819@gmail.com>
2022-09-09 21:52:31 +0900

    ユーザー管理機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'furima/index'
+  get 'items/index'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "furima#index"
+  root to: "items#index"
+  
+  
 end
