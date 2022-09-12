b5002da0684657500f055d0ddb14663c90744af1
Sk20020822 <kaito.nike0819@gmail.com>
2022-09-13 00:21:09 +0900

    ユーザー管理

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :encrypted_password,     presence: true, length: { minimum: 7 } # ここが文字数の正規表現
+  validates :last_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ここがユーザー本名全角の正規表現
+  validates :last_name_kana,       presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # ここがフリガナ全角の正規表現
+  validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ここがユーザー本名全角の正規表現
+  validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # ここがフリガナ全角の正規表現
+
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"tarou"}
+    email                 {"sample@gmail.com"}
+    password              {"1234567"}
+    encrypted_password    {"1234567"}
+    last_name             {"山田"}
+    last_name_kana        {"ヤマダ"}
+    first_name            {"太郎"}
+    first_name_kana       {"タロウ"}
+    birth_date             {"2000-01-01"}
+  end
+end
+
