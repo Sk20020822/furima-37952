401691149195a67f5062974848c28a4612e41d9c
Sk20020822 <kaito.nike0819@gmail.com>
2022-10-08 17:34:08 +0900

    修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,7 +1,7 @@
 function item_price (){
   const itemPrice  = document.getElementById("item-price");
   itemPrice.addEventListener("keyup", () => {
-    const fee = itemPrice.value * 0.1;
+    const fee = (Math.floor(itemPrice.value * 0.1));
     const addTaxPrice = document.getElementById("add-tax-price");
     addTaxPrice.innerHTML = `${fee}`;
     const SalesProfit = (Math.floor(itemPrice.value - fee));
